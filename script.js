2f7f5160fecaa9629bd59905f0cc66105c967917
Rust Skull Games <rustskullgames@gmail.com>
2026-01-16 20:46:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ const button = document.getElementById("myButton");
 button.addEventListener("click", function () {
     alert("Butona bastın Diğerine De Bas!");
 });
-
-const button = document.getElementById("myButton2");
-
-button.addEventListener("click", function () {
-    alert("Helal Olsun!");
-});
