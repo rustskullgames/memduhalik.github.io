6934f5fd2a39f16571f3550443587db6afe75de6
Rust Skull Games <rustskullgames@gmail.com>
2026-01-18 15:19:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ colorBtn.addEventListener("click", function () {
     document.body.style.backgroundColor = "#FF";
 });
 
+const colorBtn = document.getElementById("koyu");
+
+colorBtn.addEventListener("click", function () {
+    document.body.style.backgroundColor = "#00";
+});
+
