faef8aab1b38c7306d98304ab76cfe061313ab5c
Rust Skull Games <rustskullgames@gmail.com>
2026-01-22 01:04:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById("playBtn").onclick = () => {
-  window.location.href = "game.html";
-};
+ const root = document.documentElement;
+
+    document.getElementById("yellowTheme").onclick = () => {
+      root.style.setProperty("--accent", "#ffd000");
+      root.style.setProperty("--accent-soft", "rgba(255,208,0,0.15)");
+      root.style.setProperty("--accent-border", "#ffd000");
+    };
+
+    document.getElementById("greenTheme").onclick = () => {
+      root.style.setProperty("--accent", "#00ff66");
+      root.style.setProperty("--accent-soft", "rgba(0,255,102,0.15)");
+      root.style.setProperty("--accent-border", "#00ff66");
+    };
